2db92c8a1be2412d1622da1f44a42e2177546c58
francescoriccardocrescenzi <155580439+francescoriccardocrescenzi@users.noreply.github.com>
2024-02-03 08:08:50 +0100

    Add desaturation methods for RGBImage. Fix bug in the self.raw setter.
    Only normalize the raw_data array in the self.raw setter only if raw_data.dtype is an integer subtype.

--- depingi.py
+++ depingi.py
@@ -5,7 +5,8 @@ import numpy as np
 class Image:
     """Class used to open and process images.
 
-    The image is stored in raw form as numpy ndarray.
+    The image is stored in raw form as a float numpy ndarray whose values
+    are comprised between 0 and 1.
     """
 
     # CONSTANTS
@@ -29,12 +30,24 @@ class Image:
 
     @raw.setter
     def raw(self, raw_data: np.ndarray):
-        """Setter for self.raw."""
-        raw_data = raw_data.astype(np.float32)/self.UINT8MAX
+        """Setter for self.raw.
+
+        Check whether raw_data is of an accepted data type (integer or float). If raw_data is an integer array,
+        normalize it. If it is a float array, leave it as is. If raw_data is not of an accepted type,
+        raise a type error.
+        """
+        if np.issubdtype(raw_data.dtype, np.integer):
+            raw_data = raw_data.astype(np.float32)/self.UINT8MAX
+        elif np.issubdtype(raw_data.dtype, np.floating):
+            pass
+        else:
+            raise TypeError("The numpy array used to initialize an instance of Image must be either a float array" +
+                            " or an integer array.")
         self._raw = raw_data
 
     @property
     def raw_as_uint8(self):
+        """Return self.raw as a uint8 array whose values are comprised between 0 and 255."""
         return (self.raw*self.UINT8MAX).astype(np.uint8)
 
 
@@ -51,6 +64,35 @@ class RGBImage(Image):
 
     The images are stored as 3d numpy arrays.
     """
-    pass
+
+    # DESATURATION
+
+    def weighted_average_desaturated(self, weights: np.ndarray = np.array([1 / 3, 1 / 3, 1 / 3])) -> LImage:
+        """Convert to a luminance image using a weighted average of the RGB channels.
+
+        The luminance of each pixel is determined by taking the weighted average of the red, green, and blue components
+        according to the provided weights.
+
+        Arguments:
+            - weight: array in the form [wr, wg, wb], where wr, wg, wb are the weights used for the average.
+        """
+        # We use a tensor product to take the weighted average.
+        desaturated_raw = np.tensordot(self.raw, weights, axes=(2, 0))
+        return LImage(desaturated_raw)
+
+    def luminosity_desaturated(self) -> LImage:
+        """Convert to a luminance image using the luminosity method.
+
+        Apply RGBImage.weighted_average_desaturated with weights [0.21, 0.72, 0.07].
+        """
+        return self.weighted_average_desaturated(np.array([0.21, 0.72, 0.07]))
+
+    def lightness_desaturated(self) -> LImage:
+        """Convert to a luminance image using the lightness method.
+
+        The luminance of each pixel is determined as (min(R,G,B) + max(R,G,B))/2.
+        """
+        desaturated_raw = (np.amax(self.raw, axis=2) + np.amin(self.raw, axis=2)) / 2
+        return LImage(desaturated_raw)
 
 
--- test_depingi.py
+++ test_depingi.py
@@ -10,9 +10,13 @@ def open_image(file_path):
     return np.asarray(PIL.Image.open(file_path))
 
 
-def pil_image(raw_data):
+def rgb_pil_image(raw_data):
     """Create PIL image from raw data."""
     return PIL.Image.fromarray(raw_data)
+
+def l_pil_image(raw_data):
+    """Create PIL image from raw data."""
+    return PIL.Image.fromarray(raw_data, mode="L")
 
 
 class TestImage(ut.TestCase):
@@ -24,8 +28,29 @@ class TestImage(ut.TestCase):
         raw = open_image(file_path)
         im = dp.Image(raw)
         uint8raw = im.raw_as_uint8
-        pil_im = pil_image(uint8raw)
+        pil_im = rgb_pil_image(uint8raw)
         # pil_im.show()
+
+
+class TestRGBImage(ut.TestCase):
+    """Test LImage class."""
+
+    def test_desaturation_methods(self):
+        """Open a colored image and desaturate it in three different ways."""
+        file_path = "data/plants.jpg"
+        raw = open_image(file_path)
+        rgb_im = dp.RGBImage(raw)
+        l_im1 = rgb_im.weighted_average_desaturated()
+        l_im2 = rgb_im.luminosity_desaturated()
+        l_im3 = rgb_im.lightness_desaturated()
+        pil_im1 = l_pil_image(l_im1.raw_as_uint8)
+        pil_im2 = l_pil_image(l_im2.raw_as_uint8)
+        pil_im3 = l_pil_image(l_im3.raw_as_uint8)
+        pil_im4 = rgb_pil_image(rgb_im.raw_as_uint8)
+        # pil_im1.show()
+        # pil_im2.show()
+        # pil_im3.show()
+        # pil_im4.show()
 
 
 if __name__ == "__main__":
